d333f5c15e089c082424d8af7ffd694355ff0e9b
kevxiao <kevinxiao2@gmail.com>
2015-06-18 11:49:59 -0400

    Comment for deck controller

--- Controllers/DeckController.h
+++ Controllers/DeckController.h
@@ -1,3 +1,9 @@
+/* Deck controller may not be needed since it only calls
+ * methods from the deck model. The deck model can simply be
+ * used in other controllers instead of having everything go
+ * through the deck controller.
+ */
+
 #ifndef DECKCONTROLLER_H
 #define DECKCONTROLLER_H
 
