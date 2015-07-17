500c88ac61fbf6ee51904c00a89c10794765071a
MarkoNerandzic <markonerandzic@yahoo.com>
2015-07-16 21:51:12 -0400

    Added images and updated DeckGUI

--- Views/DeckGUI.cc
+++ Views/DeckGUI.cc
@@ -7,13 +7,19 @@ using std::string;
 using std::transform;
 
 const char * image_names[] = {   
-	"cards_png/0_0.png", "cards_png/0_1.png", "cards_png/0_2.png", "cards_png/0_3.png",
-	"cards_png/1_0.png", "cards_png/1_1.png", "cards_png/1_2.png", "cards_png/1_3.png",
-	"cards_png/2_0.png", "cards_png/2_1.png", "cards_png/2_2.png", "cards_png/2_3.png",
-	"cards_png/3_0.png", "cards_png/3_1.png", "cards_png/3_2.png", "cards_png/3_3.png",
-	"cards_png/4_0.png", "cards_png/4_1.png", "cards_png/4_2.png", "cards_png/4_3.png",
-	"cards_png/5_0.png", "cards_png/5_1.png", "cards_png/5_2.png", "cards_png/5_3.png",
-	"cards_png/back_1.png"
+	"Assets/0_0.png", "Assets/0_1.png", "Assets/0_2.png", "Assets/0_3.png",
+    "Assets/0_4.png", "Assets/0_5.png", "Assets/0_6.png", "Assets/0_7.png",
+    "Assets/0_8.png", "Assets/0_9.png", "Assets/0_j.png", "Assets/0_q.png", "Assets/0_k.png",
+	"Assets/1_0.png", "Assets/1_1.png", "Assets/1_2.png", "Assets/1_3.png",
+    "Assets/1_4.png", "Assets/1_5.png", "Assets/1_6.png", "Assets/1_7.png",
+    "Assets/1_8.png", "Assets/1_9.png", "Assets/1_j.png", "Assets/1_q.png", "Assets/1_k.png",
+    "Assets/2_0.png", "Assets/2_1.png", "Assets/2_2.png", "Assets/2_3.png",
+    "Assets/2_4.png", "Assets/2_5.png", "Assets/2_6.png", "Assets/2_7.png",
+    "Assets/2_8.png", "Assets/2_9.png", "Assets/2_j.png", "Assets/2_q.png", "Assets/2_k.png",
+    "Assets/3_0.png", "Assets/3_1.png", "Assets/3_2.png", "Assets/3_3.png",
+    "Assets/3_4.png", "Assets/3_5.png", "Assets/3_6.png", "Assets/3_7.png",
+    "Assets/3_8.png", "Assets/3_9.png", "Assets/3_j.png", "Assets/3_q.png", "Assets/3_k.png",
+	"Assets/nothing.png"
 }; 
 
 Glib::RefPtr<Gdk::Pixbuf> createPixbuf(const string & name) {
@@ -29,7 +35,7 @@ DeckGUI::~DeckGUI() {
 }
 
 Glib::RefPtr<Gdk::Pixbuf> DeckGUI::getCardImage( Faces f, Suits s ) {
-	int index = ((int) f)*4 + ((int) s );
+	int index = ((int) f) + ((int) s )*13;
 	return deck[ index ];
 }
 
